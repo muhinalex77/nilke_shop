f9251b509bac6afdac743e78ac845c444db98742
Alex <muhinalex77@gmail.com>
2021-04-12 00:29:12 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav__items'),
+        menuItem = document.querySelectorAll('.nav__item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav__items_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav__items_active');
+        })
+    })
+})
+
+new WOW().init();
